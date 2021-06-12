1695e6b9834e97cdf6da1abcc3d84c6b2b342977
Gowtham0404 <85618255+Gowtham0404@users.noreply.github.com>
2021-06-12 11:26:56 +0530

    Delete PDF TO TEXT.py

--- PDF TO TEXT.py
+++
@@ -1,9 +0,0 @@
-# First install the PDF package
-pip install PyPDF2
-#import the library
-import PyPDF2
-#Read the PDF file
-#inside the PDF path
-pdf = PyPDF2.PdfFileReader (open("/content/drive/MyDrive/Student_AFD_8885488_492018144248773.pdf","rb"))
-for page in pdf.pages:
-  print (page.extractText());
